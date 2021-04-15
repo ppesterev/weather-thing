58c7dc1a4f1b97687f672801a5b5b8279d1072ed
ppesterev <ppesterev@mail.ru>
2021-04-15 18:41:20 +0700

    mock app layout and some global styles

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,22 +1,80 @@
 <template>
-  <div id="app"></div>
+  <div class="app">
+    <div class="app__panel app__search"></div>
+    <div class="app__panel app__tracked-list"></div>
+    <Map class="app__panel app__map" />
+    <div class="app__panel app__location-details"></div>
+    <footer class="app__footer">(c) ppesterev</footer>
+  </div>
 </template>
 
 <script>
+import Map from "./Map.vue";
 export default {
   name: "App",
-  components: {},
+  components: { Map },
   data: () => ({})
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 16px;
+  font-family: "Segoe UI", Verdana, sans-serif;
+}
+
+body {
+  margin: 0;
+  height: 100vh;
+}
+
+.app {
+  display: grid;
+  grid-template-rows: 5fr 2fr auto;
+  grid-template-columns: 1fr 1fr 3fr;
+  grid-template-areas:
+    "search tracked map"
+    "details details details"
+    "footer footer footer";
+  grid-gap: 10px;
+
+  max-width: 1200px;
+  height: 100%;
+  margin: 0 auto;
+  padding: 10px;
+}
+
+.app__panel {
+  padding: 9px 11px 11px 9px;
+  background-color: hsl(180, 10%, 95%);
+  border-radius: 5px;
+  box-shadow: inset 2px 2px 3px 0 rgba(0, 0, 0, 0.2),
+    inset -2px -2px 3px 0 rgba(255, 255, 255, 0.6);
+}
+
+.app__map {
+  grid-area: map;
+}
+
+.app__location-search {
+  grid-area: search;
+}
+
+.app__tracked-list {
+  grid-area: tracked;
+}
+
+.app__location-details {
+  grid-area: details;
+}
+
+.app__footer {
+  grid-area: footer;
 }
 </style>
