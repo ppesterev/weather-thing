45e394f6e191d163e608f864174f70e6d7b9a9fb
ppesterev <ppesterev@mail.ru>
2021-04-17 23:40:07 +0700

    extract server data parsing functions

--- src/api.js
+++ src/api.js
@@ -5,36 +5,37 @@ const BASE_URL = "https://www.metaweather.com/api/";
 
 const getFullUrl = (endpoint) => `${PROXY_URL}${BASE_URL}${endpoint}`;
 
+const parseLocation = (locationData) => {
+  const { title, woeid, latt_long } = locationData;
+  const [latt, long] = latt_long.split(",");
+  return {
+    title,
+    woeid,
+    coords: {
+      latt: parseFloat(latt),
+      long: parseFloat(long)
+    }
+  };
+};
+
+const parseWeather = (weatherData) => ({
+  date: new Date(weatherData.applicable_date),
+  temp: weatherData.the_temp,
+  weatherType: weatherData.weather_state_name,
+  icon: `https://www.metaweather.com/static/img/weather/${weatherData.weather_state_abbr}.svg`
+});
+
 export const searchLocation = (str) => {
   return axios
     .get(getFullUrl("location/search"), { params: { query: str } })
-    .then((response) =>
-      response.data.map((result) => ({
-        title: result.title,
-        woeid: result.woeid,
-        coords: {
-          latt: parseFloat(result.latt_long.split(",")[0]),
-          long: parseFloat(result.latt_long.split(",")[1])
-        }
-      }))
-    );
+    .then((response) => response.data.map(parseLocation));
 };
 
 export const getLocationDetails = (woeid) => {
   return axios.get(getFullUrl(`location/${woeid}`)).then((response) => ({
-    location: {
-      woeid,
-      title: response.data.title,
-      coords: {
-        latt: parseFloat(response.data.latt_long.split(",")[0]),
-        long: parseFloat(response.data.latt_long.split(",")[1])
-      }
-    },
-    weather: response.data.consolidated_weather.map((weatherDay) => ({
-      date: new Date(weatherDay.applicable_date),
-      temp: weatherDay.the_temp,
-      weatherType: weatherDay.weather_state_name,
-      icon: `${BASE_URL}static/img/weather/${weatherDay.weather_state_abbr}.svg`
-    }))
+    ...parseLocation(response.data),
+    forecast: response.data.consolidated_weather
+      .map(parseWeather)
+      .sort((a, b) => a.date.getTime() - b.date.getTime())
   }));
 };
