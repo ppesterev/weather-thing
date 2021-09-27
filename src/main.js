7a7ccb67c2ca7338c090f92531ccb41de88cd007
ppesterev <ppesterev@mail.ru>
2021-09-27 07:04:59 +0700

    add vue-match-media

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from "vue";
 import App from "./components//App.vue";
 import store from "./store";
 
+import MQ from "vue-match-media/src";
+Vue.use(MQ);
+
 Vue.config.productionTip = false;
 
 new Vue({
   store,
+  mq: {
+    phone: "(max-width: 560px)",
+    tablet: "(max-width: 900px)"
+  },
   render: (h) => h(App)
 }).$mount("#app");
