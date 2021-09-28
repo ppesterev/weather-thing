0b64d569b3101052d645e1e47627be5a6c95bcf3
ppesterev <ppesterev@mail.ru>
2021-09-28 18:31:18 +0700

    base toggle styles

--- src/components/BaseToggle.vue
+++ src/components/BaseToggle.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
-    <label v-for="option in options" :key="option.value">
+  <div class="toggle">
+    <span class="toggle__option" v-for="option in options" :key="option.value">
       <input
         type="radio"
+        :id="`${inputName}-${option.value}`"
         :name="inputName"
         :value="option.value"
         :checked="option.value === selectedOption"
         @change="handleChange($event.target.checked, option.value)"
       />
-      {{ option.label }}
-    </label>
+      <label :for="`${inputName}-${option.value}`">
+        {{ option.label }}
+      </label>
+    </span>
   </div>
 </template>
 
@@ -49,4 +52,40 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.toggle,
+.toggle__option {
+  display: flex;
+}
+
+.toggle__option input {
+  position: absolute;
+  clip: rect(0 0 0 0);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+}
+
+.toggle__option label {
+  padding: 5px 8px;
+
+  color: white;
+  background-color: rgb(76, 138, 189);
+  border-radius: 3px;
+  box-shadow: 1px 2px 5px -3px inset white, -1px -2px 5px -3px inset grey;
+  cursor: pointer;
+}
+
+.toggle__option:first-of-type label {
+  border-radius: 5px 0 0 5px;
+}
+
+.toggle__option:last-of-type label {
+  border-radius: 0 5px 5px 0;
+}
+
+.toggle__option input:checked + label {
+  background-color: rgb(53, 103, 143);
+  box-shadow: -1px -2px 5px -3px inset white, 1px 2px 5px -3px inset grey;
+}
+</style>
